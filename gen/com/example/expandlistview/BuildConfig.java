cc59fb8d876f15002dbc1e1fc676192c54751cb2
traiyani <traiyani75@gmail.com>
2014-11-13 12:09:34 +0530

    ---

---
+++ gen/com/example/expandlistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.expandlistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/expandlistview/ExpandableListViewAdapter.java
@@ -0,0 +1,115 @@
+package com.example.expandlistview;
+
+
+import java.util.HashMap;
+import java.util.List;
+ 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+ 
+public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
+ 
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<String>> _listDataChild;
+ 
+    public ExpandableListViewAdapter(Context context, List<String> listDataHeader,
+            HashMap<String, List<String>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+ 
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+    	Log.d("ExpandAdapter", "in child get ");
+    	return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+ 
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+ 
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+ 
+        final String childText = (String) getChild(groupPosition, childPosition);
+        Log.d("Expandable List View", "Childview Displayed");
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_child, null);
+        }
+ 
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+ 
+        txtListChild.setText(childText);
+        return convertView;
+    }
+ 
+    @Override
+    public int getChildrenCount(int groupPosition) {
+    	Log.d("expanding", "childview called"+this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size());
+    	return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+ 
+    @Override
+    public Object getGroup(int groupPosition) {
+    	Log.d("ExpandAdapter", "in grp ");
+        return this._listDataHeader.get(groupPosition);
+    }
+ 
+    @Override
+    public int getGroupCount() {
+    	Log.d("ExpandAdapter", "in grp count");
+        return this._listDataHeader.size();
+    }
+ 
+    @Override
+    public long getGroupId(int groupPosition) {
+    	Log.d("ExpandAdapter", "in grp Id");
+        return groupPosition;
+    }
+ 
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+            View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+        	Log.d("ExpandAdapter", "in grp view");
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_group, null);
+        }
+ 
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+ 
+        return convertView;
+    }
+ 
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+ 
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
